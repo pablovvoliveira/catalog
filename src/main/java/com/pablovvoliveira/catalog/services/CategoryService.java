e6204f6255b43c3ce8fdd7575d1b8f4ead0b7d74
Pablo Oliveira <pablovvoliveira@gmail.com>
2023-06-03 11:04:56 -0300

    H2 database, test profile

---
+++ src/main/java/com/pablovvoliveira/catalog/services/CategoryService.java
@@ -0,0 +1,21 @@
+package com.pablovvoliveira.catalog.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.pablovvoliveira.catalog.entities.Category;
+import com.pablovvoliveira.catalog.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+	
+	@Autowired
+	private CategoryRepository repository;
+	
+	public List<Category> findAll() {
+		return repository.findAll();
+	}
+	
+}
