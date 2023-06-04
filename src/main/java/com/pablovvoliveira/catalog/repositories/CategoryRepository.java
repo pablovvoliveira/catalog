784fca9e2c89486a21684210a32c0d455c8ad20d
Pablo Oliveira <pablovvoliveira@gmail.com>
2023-06-04 11:26:10 -0300

    CategoryDTO

--- src/main/java/com/pablovvoliveira/catalog/repositories/CategoryRepository.java
+++ src/main/java/com/pablovvoliveira/catalog/repositories/CategoryRepository.java
@@ -1,11 +1,9 @@
 package com.pablovvoliveira.catalog.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.pablovvoliveira.catalog.entities.Category;
 
-@Repository
 public interface CategoryRepository extends JpaRepository<Category, Long>{
 
 }
--- src/main/java/com/pablovvoliveira/catalog/services/CategoryService.java
+++ src/main/java/com/pablovvoliveira/catalog/services/CategoryService.java
@@ -1,10 +1,12 @@
 package com.pablovvoliveira.catalog.services;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.pablovvoliveira.catalog.dto.CategoryDTO;
 import com.pablovvoliveira.catalog.entities.Category;
 import com.pablovvoliveira.catalog.repositories.CategoryRepository;
 
@@ -17,8 +19,10 @@ public class CategoryService {
 	private CategoryRepository repository;
 	
 	@Transactional
-	public List<Category> findAll() {
-		return repository.findAll();
+	public List<CategoryDTO> findAll() {
+		List<Category> list = repository.findAll();		
+		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
+				
 	}
 	
 }
